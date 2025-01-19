6b836d5b38cea1b672590bc5d6c15ab7a6a74591
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-19 19:21:47 +0600

    update offered course and admin route

--- client/src/routes/faculty.routes.tsx
+++ client/src/routes/faculty.routes.tsx
@@ -1,6 +1,5 @@
 import FacultyDashboard from '../pages/faculty/FacultyDashboard';
-import MyCourses from '../pages/faculty/MyCourses';
-import MyStudents from '../pages/faculty/MyStudents';
+import OfferedCourse from '../pages/faculty/OfferedCourse';
 export const facultyPaths = [
   {
     name: 'Dashboard',
@@ -8,12 +7,8 @@ export const facultyPaths = [
     element: <FacultyDashboard />,
   },
   {
-    name: 'My Courses',
-    path: 'courses',
-    element: <MyCourses />,
-  },
-  {
-    path: 'courses/:registerSemesterId/:courseId',
-    element: <MyStudents />,
+    name: 'Offered Course',
+    path: 'offered-course',
+    element: <OfferedCourse />,
   },
 ];
