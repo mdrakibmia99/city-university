2ec00d961d52fbeb58c664534677c16bd7aa5122
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-19 19:18:34 +0600

    update protected route

--- client/src/components/layout/ProtectedRoute.tsx
+++ client/src/components/layout/ProtectedRoute.tsx
@@ -1,33 +1,11 @@
 import { ReactNode } from 'react';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import {
-  logout,
-  selectCurrentUser,
-  useCurrentToken,
-} from '../../redux/features/auth/authSlice';
+import { useAppSelector } from '../../redux/hooks';
+import { useCurrentToken } from '../../redux/features/auth/authSlice';
 import { Navigate } from 'react-router-dom';
-import { verifyToken } from '../../utils/verifyToken';
 
-type TProtectedRoute = {
-  children: ReactNode;
-  role: string | undefined;
-};
-
-const ProtectedRoute = ({ children, role }: TProtectedRoute) => {
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const token = useAppSelector(useCurrentToken);
 
-  let user;
-
-  if (token) {
-    user = verifyToken(token);
-  }
-
-  const dispatch = useAppDispatch();
-
-  if (role !== undefined && role !== user?.role) {
-    dispatch(logout());
-    return <Navigate to="/login" replace={true} />;
-  }
   if (!token) {
     return <Navigate to="/login" replace={true} />;
   }
