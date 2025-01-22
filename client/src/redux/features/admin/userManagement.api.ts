aa999988a394c638ab6a93c379cff8aa9c00300f
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-22 16:14:30 +0600

    update user management

--- client/src/redux/features/admin/userManagement.api.ts
+++ client/src/redux/features/admin/userManagement.api.ts
@@ -28,30 +28,6 @@ const userManagementApi = baseApi.injectEndpoints({
         };
       },
     }),
-    getAllFaculties: builder.query({
-      query: (args) => {
-        console.log(args);
-        const params = new URLSearchParams();
-
-        if (args) {
-          args.forEach((item: TQueryParam) => {
-            params.append(item.name, item.value as string);
-          });
-        }
-
-        return {
-          url: '/faculties',
-          method: 'GET',
-          params: params,
-        };
-      },
-      transformResponse: (response: TResponseRedux<TStudent[]>) => {
-        return {
-          data: response.data,
-          meta: response.meta,
-        };
-      },
-    }),
     addStudent: builder.mutation({
       query: (data) => ({
         url: '/users/create-student',
@@ -59,19 +35,8 @@ const userManagementApi = baseApi.injectEndpoints({
         body: data,
       }),
     }),
-    changePassword: builder.mutation({
-      query: (data) => ({
-        url: '/auth/change-password',
-        method: 'POST',
-        body: data,
-      }),
-    }),
   }),
 });
 
-export const {
-  useAddStudentMutation,
-  useGetAllStudentsQuery,
-  useGetAllFacultiesQuery,
-  useChangePasswordMutation,
-} = userManagementApi;
+export const { useAddStudentMutation, useGetAllStudentsQuery } =
+  userManagementApi;
